1bf72b0e0c6f9326ece787be6dc2865ea14dc257
smoaty <steven.moats@gmail.com>
2017-10-03 17:39:28 +0000

    "Drum machine project"

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	if(!audio) return
+		
+	
+		audio.currentTime = 0
+		audio.play()
+		key.classList.add('playing')
+}
+
+
+function endSound(e){
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	if(!key) return
+	key.classList.remove('playing')
+}
+
+
+
+
+window.addEventListener('keydown', playSound)
+window.addEventListener('keyup', endSound)
